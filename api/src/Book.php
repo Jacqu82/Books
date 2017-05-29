6fc3bdce36d0d6876956812c1b8791098ae64f95
Jacqu82 <jacqu25@yahoo.com>
2017-05-29 21:59:24 +0200

    class Book and database

--- api/src/Book.php
+++ api/src/Book.php
@@ -1,8 +1,72 @@
 <?php
 
-class Book
+class Book implements JsonSerializable
 {
 
-    
+    private $id;
+    private $name;
+    private $author;
+    private $description;
+
+
+    public function __construct()
+    {
+        $this->id = -1;
+        $this->name = '';
+        $this->author = '';
+        $this->description = '';
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'author' => $this->author,
+            'description' => $this->description
+        ];
+    }
+
 
 }
