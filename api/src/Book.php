fac7361253b1ed8fd0c0831a0c076af4f2092605
Jacqu82 <jacqu25@yahoo.com>
2017-05-30 16:39:48 +0200

    class Book

--- api/src/Book.php
+++ api/src/Book.php
@@ -56,7 +56,90 @@ class Book implements JsonSerializable
         $this->description = $description;
     }
 
+    public function loadFromDB(mysqli $connection, $id)
+    {
+        $id = $connection->real_escape_string($id);
 
+        $sql = /** @lang text */
+            "SELECT * FROM books WHERE id = $id";
+
+        $result = $connection->query($sql);
+
+        if ($result == true && $result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            $this->author = $row['author'];
+            $this->description = $row['description'];
+
+            return true;
+        }
+        return false;
+    }
+
+    public function create(mysqli $connection, $name, $author, $description)
+    {
+        $name = $connection->real_escape_string($name);
+        $author = $connection->real_escape_string($author);
+        $description = $connection->real_escape_string($description);
+
+        $sql = /** @lang text */
+            "INSERT INTO books (name, author, description) VALUES ('$name', '$author', '$description')";
+
+        $result = $connection->query($sql);
+        if ($result) {
+
+            $this->id = $connection->insert_id;
+            $this->name = $name;
+            $this->author = $author;
+            $this->description = $description;
+
+            return true;
+        }
+        return false;
+    }
+
+    public function update(mysqli $connection, $name, $author, $description)
+    {
+        $id = $connection->real_escape_string($this->id);
+        $name = $connection->real_escape_string($name);
+        $author = $connection->real_escape_string($author);
+        $description = $connection->real_escape_string($description);
+
+        $sql = /** @lang text */
+            "UPDATE books SET name = '$name', author = '$author', description = '$description' WHERE id = $id";
+
+        $result = $connection->query($sql);
+
+        if ($result) {
+            $this->name = $name;
+            $this->author = $author;
+            $this->description = $description;
+
+            return true;
+        }
+        return false;
+    }
+
+    public function deleteFromDB(mysqli $connection)
+    {
+        $id = $connection->real_escape_string($this->id);
+
+        $sql = /** @lang text */
+            "DELETE FROM books WHERE id = $id";
+
+        $result = $connection->query($sql);
+        if ($result) {
+            $this->id = -1;
+            $this->name = '';
+            $this->author = '';
+            $this->description = '';
+
+            return true;
+        }
+        return false;
+    }
 
     public function jsonSerialize()
     {
@@ -67,6 +150,4 @@ class Book implements JsonSerializable
             'description' => $this->description
         ];
     }
-
-
 }
